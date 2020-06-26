9336fb653cd70e47fbe1ef1c219f7fdc9ce16f52
teamfam <teamfam2014@gmail.com>
2020-06-26 00:30:17 -0400

    initial version of the rest tool

---
+++ rest-tool/src/test/java/com/teamfam/sprintme/resttool/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamfam.sprintme.resttool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
