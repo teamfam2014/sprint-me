9666cd20c1c5bf47f2a618a36bbb563cd4c5ddfd
teamfam <teamfam2014@gmail.com>
2020-06-26 00:44:01 -0400

    Added new auto configuration class for sprint me

---
+++ rest-tool/src/main/java/com/teamfam/sprintme/rest/autoconfig/SprintMeRestAutoConfiguration.java
@@ -0,0 +1,16 @@
+package com.teamfam.sprintme.rest.autoconfig;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Auto configure the rest template.
+ */
+@Configuration
+@ComponentScan("com.teamfam.sprintme.rest")
+@ConditionalOnClass(RestTemplateBuilder.class)
+public class SprintMeRestAutoConfiguration {
+    
+}
