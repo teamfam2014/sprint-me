4c38e663228e44ca17645784a0888da89a2d0213
teamfam <teamfam2014@gmail.com>
2020-06-26 13:39:31 -0400

    updated lookup path

--- rest-tool/src/main/java/com/teamfam/sprintme/sdk/autoconfig/SprintMeRestAutoConfiguration.java
+++ rest-tool/src/main/java/com/teamfam/sprintme/sdk/autoconfig/SprintMeRestAutoConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
  * Auto configure the rest template.
  */
 @Configuration
-@ComponentScan("com.teamfam.sprintme.rest")
+@ComponentScan("com.teamfam.sprintme.sdk.rest")
 @ConditionalOnClass(RestTemplateBuilder.class)
 public class SprintMeRestAutoConfiguration {
     
