acb7b8bd61125bf597cd902b622479d5155ddab8
teamfam2014 <teamfam2014@gmail.com>
2020-10-05 00:11:51 -0400

    define rpc method

--- grpc-tool/src/main/java/com/teamfam/sprintme/grpc/GrpcClient.java
+++ grpc-tool/src/main/java/com/teamfam/sprintme/grpc/GrpcClient.java
@@ -1,5 +1,18 @@
 package com.teamfam.sprintme.grpc;
 
-public interface GrpcClient {
-    
+/**
+ * Represent a gRpc Client used for any gRPC request/response.
+ * 
+ * @author teamfam
+ */
+public interface GrpcClient{
+
+    /**
+     * Invoke a remote method via RPC by passing in the RPC request as well as providing an RPC 
+     * response.
+     * 
+     * @param grpcRequest The gRPC Request to make to the remote gRPC Server.
+     * @param grpcResponseType The gRPC response type from the RPC server.
+     */
+    public <REQ,RES> RES invokeMethod(REQ grpcRequest,Class<RES> grpcResponseType);
 }
