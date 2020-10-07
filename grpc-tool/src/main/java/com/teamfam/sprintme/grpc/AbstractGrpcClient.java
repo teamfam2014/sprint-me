ea3b6ec70301b63ffefb1109f9eee938b100fb0e
teamfam <teamfam2014@gmail.com>
2020-10-07 16:33:12 -0400

    implemented abstract grpc

--- grpc-tool/src/main/java/com/teamfam/sprintme/grpc/AbstractGrpcClient.java
+++ grpc-tool/src/main/java/com/teamfam/sprintme/grpc/AbstractGrpcClient.java
@@ -1,5 +1,12 @@
 package com.teamfam.sprintme.grpc;
 
-public abstract class AbstractGrpcClient implements GrpcClient{
+public abstract class AbstractGrpcClient implements GrpcClient {
+
+    @Override
+    public <REQ, RES> RES invokeMethod(REQ grpcRequest, Class<RES> grpcResponseType) {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
+    public abstract <STUB> STUB invokeStub(Class<STUB> stubType);
 }
