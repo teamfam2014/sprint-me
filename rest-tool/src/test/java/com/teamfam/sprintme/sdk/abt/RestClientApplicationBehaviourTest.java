8b6bc4f412b114438d36fa4067935e6864283d0c
teamfam <teamfam2014@gmail.com>
2020-10-20 00:18:30 -0400

    added http post

--- rest-tool/src/test/java/com/teamfam/sprintme/sdk/abt/RestClientApplicationBehaviourTest.java
+++ rest-tool/src/test/java/com/teamfam/sprintme/sdk/abt/RestClientApplicationBehaviourTest.java
@@ -25,6 +25,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -50,29 +51,49 @@ public class RestClientApplicationBehaviourTest{
 	@DisplayName("Simple Http Get")
 	@Test
 	public void testPlainHttpGet() throws IOException{
-		validateRestOperation(simpleHttpGet(),"/stubs/account.json","http://localhost:8080/account/{id}",simplePathParameters());
+		validateRestOperation(simpleHttpGet(),"/stubs/account.json","http://localhost:8080/account/{id}");
 	}
 	
 	@DisplayName("Loaded Http Get")
 	@Test
 	public void testLoadedHttpGet() throws IOException{
-		validateRestOperation(loadedHttpGet(),"/stubs/account.json","http://localhost:8080/account/{id}?paid=true",simplePathParameters());
+		validateRestOperation(loadedHttpGet(),"/stubs/account.json","http://localhost:8080/account/{id}?paid=true");
 	}
 
-	private void validateRestOperation(RestClientDto<?> request,String fileName,String httpUrlString,MultiValueMap<String,String> pathParameters) throws IOException{
+	@DisplayName("Simple Http Post")
+	@Test
+	public void testSimpleHttpPost() throws IOException{
+		validateRestOperation(simpleHttpPost(),"/stubs/account.json","http://localhost:8080/account/{id}");
+	}
+
+	@DisplayName("Loaded Http Post")
+	@Test
+	public void testLoadedHttpPost() throws IOException{
+		validateRestOperation(loadedHttpPost(),"/stubs/account.json","http://localhost:8080/account/{id}?paid=true");
+	}	
+
+	
+
+	private void validateRestOperation(RestClientDto<?> request,String fileName,String httpUrlString) throws IOException{
 		//ARRANGE
 		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(httpUrlString).encode();
 		String url = null;
-		if (pathParameters != null && !pathParameters.isEmpty()){
+		if (request.getPathParameters() != null && !request.getPathParameters().isEmpty()){
 			url = builder.buildAndExpand(simplePathParameters())
 						 .toUriString();
 		}else{
 			url = builder.build()
 						 .toUriString();
 		}
-		String simpleGetResponseStub = FileUtils.readFileToString(new File(classpath + "/" + fileName), "UTF-8");
-		mockRestServiceServer.expect(once(), requestTo(url))
-							 .andRespond(withSuccess(simpleGetResponseStub,MediaType.APPLICATION_JSON));
+		String simpleResponseStub = FileUtils.readFileToString(new File(classpath + "/" + fileName), "UTF-8");
+		if (request.getPayload() != null){
+			mockRestServiceServer.expect(once(), requestTo(url))
+								.andExpect(MockRestRequestMatchers.content().string(request.getPayload().toString()))
+								.andRespond(withSuccess(simpleResponseStub,MediaType.APPLICATION_JSON));
+		}else{
+			mockRestServiceServer.expect(once(), requestTo(url))
+								.andRespond(withSuccess(simpleResponseStub,MediaType.APPLICATION_JSON));
+		}
 		//ACT
 		Account account = restClientSdk.send(request, Account.class);
 		//ASSERT
@@ -104,6 +125,36 @@ public class RestClientApplicationBehaviourTest{
 											.pathParameters(simplePathParameters())
 											.queryParameters(simpleQueryParameters())
 											.build();
+	}
+
+	private RestClientDto<?> simpleHttpPost() throws IOException{
+		return RestClientDto.builder().headers(simpleHeaders())
+											.host("localhost")
+											.port(8080)
+											.scheme(HttpScheme.HTTP)
+											.httpMethod(HttpMethod.POST)
+											.uri("/account/{id}")
+											.pathParameters(simplePathParameters())
+											.payload(simplePayload())
+											.build();
+	}
+	
+	private RestClientDto<?> loadedHttpPost() throws IOException{
+		return RestClientDto.builder().headers(simpleHeaders())
+											.host("localhost")
+											.port(8080)
+											.scheme(HttpScheme.HTTP)
+											.httpMethod(HttpMethod.POST)
+											.uri("/account/{id}")
+											.pathParameters(simplePathParameters())
+											.queryParameters(simpleQueryParameters())
+											.payload(simplePayload())
+											.build();
+	}
+
+	private String simplePayload() throws IOException{
+		String payload = FileUtils.readFileToString(new File(classpath + "/" + "/stubs/account.json"), "UTF-8");
+		return payload;
 	}
 
 	private Map<String,String> simpleHeaders(){
